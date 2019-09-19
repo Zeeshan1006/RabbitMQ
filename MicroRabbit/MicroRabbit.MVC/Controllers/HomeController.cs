61995b5a6092c716ed63c86a748bf7c2842bf127
Zeeshan <mohd.zeeshan@mail.vinove.com>
2019-09-19 19:09:17 +0530

    Presentation layer

---
+++ MicroRabbit/MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Models.DTO;
+using MicroRabbit.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransferService transferService;
+
+        public object TransferDTO { get; private set; }
+
+        public HomeController(ITransferService transferService)
+        {
+            this.transferService = transferService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDTO transfer = new TransferDTO()
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount = model.TransferAmount
+            };
+
+            await transferService.Transfer(transfer);
+            return View("Index");
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,26 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            this.apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDTO transfer)
+        {
+            var uri = "https://localhost:5001/api/banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transfer),
+                System.Text.Encoding.UTF8, "application/json");
+            var response = await apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
